c3ebc68172179922b25e48ffee7b1c475c071046
Julian Rodriguez <julian.rodriguez.ugolini@gmail.com>
2020-05-25 19:36:21 +0200

    Navigation

--- src/main/java/ch/bfh/btx8081/gui/doctor/PatientInfoView.java
+++ src/main/java/ch/bfh/btx8081/gui/doctor/PatientInfoView.java
@@ -1,6 +1,7 @@
 package ch.bfh.btx8081.gui.doctor;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
@@ -25,74 +26,70 @@ public class PatientInfoView extends HorizontalLayout implements PatientInfoInte
 
 	private ArrayList<PatientInfoListener> listeners = new ArrayList<PatientInfoListener>();
 
-	List<Patient> patients = null;
+	private List<Patient> patients = null;
 	Patient patient = null;
-	
+
 	private final VerticalLayout layout;
 	private DoctorService service = null;
-	private PersistenceManager persistenceManager = null; 
-	
+	private PersistenceManager persistenceManager = null;
+
 	public PatientInfoView() throws WrongPasswordException, UserNotFoundException {
-		
+
 		this.patient = new Patient();
 		this.persistenceManager = new PersistenceManager();
 		this.patients = persistenceManager.getPatients();
-		
+
 		final EditPatientView patientShowLayout = new EditPatientView();
-        layout = new VerticalLayout();
-                      
-       
-        for(Patient p : patients) {
-        	System.out.println(p.getUserName());
-        	if(p.getUserName().equals("natalya"))
-        	{
-        		this.patient = p;
-        	}
-        }
-        
-        patientShowLayout.setFirstName(patient.getFirstName());
-        patientShowLayout.setLastName(patient.getLastName());
-        patientShowLayout.setPhoneNumber(patient.getPhoneNumber());
-        patientShowLayout.setEMail(patient.geteMail());
-        patientShowLayout.setDoctor(patient.getDoctor().getFirstName());   
- 
-        
-        // Build a footer, add Save and Cancel buttons
-        final HorizontalLayout footer = new HorizontalLayout();
+		layout = new VerticalLayout();
 
-        // Browser page is updated
-        Button buttonCancel = new Button("Cancel"); 
-        buttonCancel.addClickListener(event -> UI.getCurrent().getPage().reload());
-        
-        // Changed fields will be saved and browser page is updated
-        Button buttonEdit = new Button("Edit", event -> {
-            try {
-            	service.changeMainInfo(patientShowLayout.getMainInfo().getValue());
-            	
-            	service.changeContactInfo(patientShowLayout.getFirstName().getValue(), 
-            			                  patientShowLayout.getLastName().getValue(), 
-            			                  patientShowLayout.getPhoneNumber().getValue(), 
-            			                  patientShowLayout.getEMail().getValue());
-            	
-            	service.setConditionAutomaticAlarm(patientShowLayout.getConditionAutomaticAlarm().getValue());
-            	
-            	System.out.println(patientShowLayout.getMainInfo().getValue());
-            	System.out.println(patientShowLayout.getFirstName().getValue());
-            	
-            	UI.getCurrent().getPage().reload();
-            	
-            } catch (final Exception e) {
-                e.printStackTrace();
-            }
-        });
+		for (Patient p : patients) {
+			System.out.println(p.getUserName());
+			if (p.getUserName().equals("natalya")) {
+				this.patient = p;
+			}
+		}
 
-        footer.getThemeList().set("spacing", true);
-        footer.add(buttonEdit, buttonCancel);
-        layout.add(patientShowLayout, footer);
-        add(layout);
-        expand(layout);
+		patientShowLayout.setFirstName(patient.getFirstName());
+		patientShowLayout.setLastName(patient.getLastName());
+		patientShowLayout.setPhoneNumber(patient.getPhoneNumber());
+		patientShowLayout.setEMail(patient.geteMail());
+		patientShowLayout.setDoctor(patient.getDoctor().getFirstName());
+
+		// Build a footer, add Save and Cancel buttons
+		final HorizontalLayout footer = new HorizontalLayout();
+
+		// Browser page is updated
+		Button buttonCancel = new Button("Cancel");
+		buttonCancel.addClickListener(event -> UI.getCurrent().getPage().reload());
+
+		// Changed fields will be saved and browser page is updated
+		Button buttonEdit = new Button("Edit", event -> {
+			try {
+				service.changeMainInfo(patientShowLayout.getMainInfo().getValue());
+
+				service.changeContactInfo(patientShowLayout.getFirstName().getValue(),
+						patientShowLayout.getLastName().getValue(), patientShowLayout.getPhoneNumber().getValue(),
+						patientShowLayout.getEMail().getValue());
+
+				service.setConditionAutomaticAlarm(patientShowLayout.getConditionAutomaticAlarm().getValue());
+
+				System.out.println(patientShowLayout.getMainInfo().getValue());
+				System.out.println(patientShowLayout.getFirstName().getValue());
+
+				UI.getCurrent().getPage().reload();
+
+			} catch (final Exception e) {
+				e.printStackTrace();
+			}
+		});
+
+		footer.getThemeList().set("spacing", true);
+		footer.add(buttonEdit, buttonCancel);
+		layout.add(patientShowLayout, footer);
+		add(layout);
+		expand(layout);
 	}
-	
+
 	@Override
 	public void setPatient() {
 		// TODO Auto-generated method stub
@@ -103,4 +100,5 @@ public class PatientInfoView extends HorizontalLayout implements PatientInfoInte
 	public void addListener(PatientInfoListener presenter) {
 		listeners.add(presenter);
 
+	}
 }
