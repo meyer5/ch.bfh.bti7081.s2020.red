6bdeba65374ecb3a3a90642fb75ebb3feeca2bc2
Julian Rodriguez <julian.rodriguez.ugolini@gmail.com>
2020-05-26 18:15:14 +0200

    Navigation

--- src/main/java/ch/bfh/btx8081/gui/doctor/PatientInfoView.java
+++ src/main/java/ch/bfh/btx8081/gui/doctor/PatientInfoView.java
@@ -19,8 +19,6 @@ import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 
-import ch.bfh.btx8081.exceptions.UserNotFoundException;
-import ch.bfh.btx8081.exceptions.WrongPasswordException;
 import ch.bfh.btx8081.model.Patient;
 
 //Overview of the addiction for the patient. Shows different data
@@ -31,12 +29,6 @@ public class PatientInfoView extends VerticalLayout implements PatientInfoInterf
 	public static final String TITLE = "Patient Info";
 
 	private PatientInfoListener presenter;
-
-//	private List<Patient> patients = null;
-//	Patient patient = null;
-//	private VerticalLayout layout;
-//	private DoctorService service = null;
-//	private PersistenceManager persistenceManager = null;
 
 	// Create Layouts
 	private HorizontalLayout hLayout = new HorizontalLayout();
@@ -49,12 +41,12 @@ public class PatientInfoView extends VerticalLayout implements PatientInfoInterf
 	private Label fixPatientFName = new Label("First name:");
 	private Label fixPatientLName = new Label("Last name:");
 	private Label fixAddictionLbl = new Label("Addiction:");
-	
+
 	// Label right
 	private Label patientFName = new Label("First name");
 	private Label patientLName = new Label("Last name");
 	private Label addictionLbl = new Label("Addiction");
-	
+
 	// Label Info
 	private Label fixInfo = new Label("Info:");
 	private Label infoLbl = new Label("Info");
@@ -86,11 +78,11 @@ public class PatientInfoView extends VerticalLayout implements PatientInfoInterf
 		Button logOutButton = new Button("Log out", event -> {
 			presenter.hadleLogOutClick();
 		});
-		
+
 		Button backButton = new Button("Back", event -> {
 			presenter.hadleBackClick();
 		});
-		
+
 		// Board
 		Board board = new Board();
 		board.addRow(getEntryOverview());
@@ -108,110 +100,56 @@ public class PatientInfoView extends VerticalLayout implements PatientInfoInterf
 
 		hLayout.setWidthFull();
 		hLayout.add(vLayout1, vLayout2, vLayout3, vLayout4);
-		
-		add(new H2("Patient overview"), hLayout, board);
 
-//		this.patient = new Patient();
-//		this.persistenceManager = new PersistenceManager();
-//		this.patients = persistenceManager.getPatients();
-//
-//		final EditPatientView patientShowLayout = new EditPatientView();
-//		layout = new VerticalLayout();
-//
-//		for (Patient p : patients) {
-//			System.out.println(p.getUserName());
-//			if (p.getUserName().equals("natalya")) {
-//				this.patient = p;
-//			}
+		add(new H2("Patient overview"), hLayout, board);
 	}
 
-//		patientShowLayout.setFirstName(patient.getFirstName());
-//		patientShowLayout.setLastName(patient.getLastName());
-//		patientShowLayout.setPhoneNumber(patient.getPhoneNumber());
-//		patientShowLayout.setEMail(patient.geteMail());
-//		patientShowLayout.setDoctor(patient.getDoctor().getFirstName());
-//
-//		// Build a footer, add Save and Cancel buttons
-//		final HorizontalLayout footer = new HorizontalLayout();
-//
-//		// Browser page is updated
-//		Button buttonCancel = new Button("Cancel");
-//		buttonCancel.addClickListener(event -> UI.getCurrent().getPage().reload());
-//
-//		// Changed fields will be saved and browser page is updated
-//		Button buttonEdit = new Button("Edit", event -> {
-//			try {
-//				service.changeMainInfo(patientShowLayout.getMainInfo().getValue());
-//
-//				service.changeContactInfo(patientShowLayout.getFirstName().getValue(),
-//						patientShowLayout.getLastName().getValue(), patientShowLayout.getPhoneNumber().getValue(),
-//						patientShowLayout.getEMail().getValue());
-//
-//				service.setConditionAutomaticAlarm(patientShowLayout.getConditionAutomaticAlarm().getValue());
-//
-//				System.out.println(patientShowLayout.getMainInfo().getValue());
-//				System.out.println(patientShowLayout.getFirstName().getValue());
-//
-//				UI.getCurrent().getPage().reload();
-//
-//			} catch (final Exception e) {
-//				e.printStackTrace();
-//			}
-//		});
-//
-//		footer.getThemeList().set("spacing", true);
-//		footer.add(buttonEdit, buttonCancel);
-//		layout.add(patientShowLayout, footer);
-//		add(layout);
-//		expand(layout);
-//	}
-	
 	// Returns a graph that shows, when a entry was made or not
-		private Chart getEntryOverview() {
-			Chart chart = new Chart();
-			chart.setHeight("10%");
-			Configuration conf = chart.getConfiguration();
+	private Chart getEntryOverview() {
+		Chart chart = new Chart();
+		chart.setHeight("10%");
+		Configuration conf = chart.getConfiguration();
 
-			conf.getChart().setZoomType(Dimension.XY);
-			conf.setTitle("Entry Overview");
+		conf.getChart().setZoomType(Dimension.XY);
+		conf.setTitle("Entry Overview");
 
-			XAxis x = new XAxis();
-			Calendar calendar = Calendar.getInstance();
-			LocalDate now = LocalDate.now();
-			calendar.set(now.getYear(), now.getMonthValue()-1, now.getDayOfMonth());
-			int totalDays = calendar.getActualMaximum(Calendar.DAY_OF_MONTH);
-			String[] days = new String[totalDays];
-			for (int i = 0; i < totalDays; i++)
-				days[i] = String.valueOf(i + 1);
-			x.setCategories(days);
-			conf.addxAxis(x);
+		XAxis x = new XAxis();
+		Calendar calendar = Calendar.getInstance();
+		LocalDate now = LocalDate.now();
+		calendar.set(now.getYear(), now.getMonthValue() - 1, now.getDayOfMonth());
+		int totalDays = calendar.getActualMaximum(Calendar.DAY_OF_MONTH);
+		String[] days = new String[totalDays];
+		for (int i = 0; i < totalDays; i++)
+			days[i] = String.valueOf(i + 1);
+		x.setCategories(days);
+		conf.addxAxis(x);
 
-			YAxis y1 = new YAxis();
-			y1.setShowEmpty(true);
-			y1.setOpposite(false);
-			y1.setShowFirstLabel(false);
-			y1.setShowLastLabel(false);
-			y1.setAllowDecimals(false);
-			y1.setCeiling(1);
-			y1.setOrdinal(true);
-			y1.setExtremes(1, 1);
-			Labels yLabels = y1.getLabels();
-			yLabels.setEnabled(false);
-			y1.setClassName("y1");
-			conf.addyAxis(y1);
+		YAxis y1 = new YAxis();
+		y1.setShowEmpty(true);
+		y1.setOpposite(false);
+		y1.setShowFirstLabel(false);
+		y1.setShowLastLabel(false);
+		y1.setAllowDecimals(false);
+		y1.setCeiling(1);
+		y1.setOrdinal(true);
+		y1.setExtremes(1, 1);
+		Labels yLabels = y1.getLabels();
+		yLabels.setEnabled(false);
+		y1.setClassName("y1");
+		conf.addyAxis(y1);
 
-			DataSeries series = new DataSeries();
-			PlotOptionsColumn plotOptionsColumn = new PlotOptionsColumn();
-			series.setPlotOptions(plotOptionsColumn);
-			series.setName("Created Entries");
+		DataSeries series = new DataSeries();
+		PlotOptionsColumn plotOptionsColumn = new PlotOptionsColumn();
+		series.setPlotOptions(plotOptionsColumn);
+		series.setName("Created Entries");
 
-			series.setyAxis(0);
-			series.setData(1, 3, 1, 0, 1, 1, 1, 0, 0, 0, 0, 1, 1, 2, 0, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1);
+		series.setyAxis(0);
+		series.setData(1, 3, 1, 0, 1, 1, 1, 0, 0, 0, 0, 1, 1, 2, 0, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1);
 
-			conf.addSeries(series);
+		conf.addSeries(series);
 
-			return chart;
-		}
+		return chart;
+	}
 
 	@Override
 	public void addListener(PatientInfoListener presenter) {
